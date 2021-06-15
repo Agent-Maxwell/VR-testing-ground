7438c76d35cbb2949c986869863ff7898948761d
Agent_Maxwell <avonbraunbehrens@gmail.com>
2021-06-15 12:42:53 -0700

    more random base code

---
+++ VR testing ground/Assets/HandPresence.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacteristics;
+    private InputDevice targetDevice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        foreach (var item in devices)
+        {
+
+        }
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+
+        if (primaryButtonValue)
+        {
+
+        }
+
+        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+
+        if (triggerValue > 0.1)
+        {
+
+        }
+
+        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+
+        if (primary2DAxisValue != Vector2.zero)
+        {
+
+        }
+    }
+}
